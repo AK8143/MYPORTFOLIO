5ff883c000616578b9851f467eab85975aa7a2ad
Anilkumar Guntupalli <ad20ado@herts.ac.uk>
2023-08-10 11:12:31 +0100

    added changes in css and html

--- force-app/main/default/lwc/bMICALCULATOR/bMICALCULATOR.js
+++ force-app/main/default/lwc/bMICALCULATOR/bMICALCULATOR.js
@@ -1,3 +1,24 @@
 import { LightningElement } from 'lwc';
 
-export default class BMICALCULATOR extends LightningElement {}
+export default class BMICALCULATOR extends LightningElement {
+ height=''
+
+weight=''
+inputhandler(event){
+const {name, value} =event.target
+if (name==="height") {
+    this.height=value
+    
+}
+if (name=="weight") {
+    this.weight=value
+}
+}
+submithandler(event){
+    event.preventDefault()
+    console.log("height", this.height)
+    console.log("weight",this.weight)
+}
+
+
+}
